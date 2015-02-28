23e43e605ff27553ebab4a3919d3a8a20fab6f5f
Patrick Blaine <pblaine@my.smccd.edu>
2015-02-27 21:22:55 -0800

    reset the templates

--- cloud/app.js
+++ cloud/app.js
@@ -1,27 +1,11 @@
-
-// These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
 var app = express();
 
-// Global app configuration section
-app.set('views', 'cloud/views');  // Specify the folder to find templates
-app.set('view engine', 'ejs');    // Set the template engine
-app.use(express.bodyParser());    // Middleware for reading request body
+// basic auth not enabled for this route
 
-// Renders the root of the app
-app.get('/home', function(req, res) {
-  res.render('home');
-});
-
-// Accepts an email address to be saved from the landing page
-app.post('/send_email', function(req, res) {
-  var Email = Parse.Object.extend("Email");
-  var email = new Email({ email: req.body.email });
-  email.save().then(function() {
-    res.render('home', { message: "Congrats, we'll contact you when we release!" });
-  }, function() {
-    res.send('Oops, something went wrong!');
-  });
-});
+// basic auth is enabled for this route
+app.get('/test',
+        express.basicAuth('YOUR_USERNAME', 'YOUR_PASSWORD'),
+        function(req, res) {});
 
 app.listen();
